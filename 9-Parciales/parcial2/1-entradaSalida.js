7988fbd45369b9c8ca872be83e04061be0f31aef
rarenix <silvinazarego@gmail.com>
2018-02-06 12:26:14 -0300

    doc

--- 9-Parciales/parcial2/1-entradaSalida.js
+++ 9-Parciales/parcial2/1-entradaSalida.js
@@ -1,6 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
+	var perimetro
+	perimetro = document.getElementById("lado").value;
+	perimetro = parseInt()
 	alert("Funciona 1-EntradaSalida");
 	
 }
