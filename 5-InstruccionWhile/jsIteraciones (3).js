6b1cbcab2994ac300604359644de190457751029
rarenix <silvinazarego@gmail.com>
2018-02-19 11:44:11 -0300

    Signed-off-by: rarenix <silvinazarego@gmail.com>

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,6 +2,7 @@ function Mostrar()
 {
 
 var clave = prompt("ingrese el número clave.");
-while (clave!="utn750");
+while (clave != "utn750")
+clave = prompt("error ingrese clave");
 
 }//FIN DE LA FUNCIÓN
