fd82223bd44df235244f94dc8f9bc376f2a2cc4b
rarenix <silvinazarego@gmail.com>
2018-02-27 12:22:11 -0300

    Signed-off-by: rarenix <silvinazarego@gmail.com>

--- 9-Parciales/recuperatorio2/4-if.js
+++ 9-Parciales/recuperatorio2/4-if.js
@@ -1,7 +1,16 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
-{
-	alert("Funciona 4-if");
-	
+{	var num1
+	var num2
+	var resultado	
+	num1 = prompt ("ingrese 1º numero ");
+	num2 = prompt("ingrese 2º numero ");
+	num1 = parseInt(num1); 
+	num2 = parseInt(num2);
+}   if(num1 == num2 ){
+	resultado = num1*num2;
+}	else if (num1 < num2){
+	resultado = num1+num2;
+
 }
 
